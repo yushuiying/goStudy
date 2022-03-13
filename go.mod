6750d0e049c0c0723e25ef2bc42ae31b541695a9
yushuiying <1986028052@qq.com>
2022-03-13 21:21:17 +0800

    feat: SayHello现在支持给指定的人打招呼啦

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/yushuiying/goStudy
+module github.com/yushuiying/goStudy/v2
 
 go 1.17
--- hello.go
+++ hello.go
@@ -2,6 +2,6 @@ package gostudy
 
 import "fmt"
 
-func SayHello() {
-	fmt.Println("你好，我是yushuiying，很高兴认识你")
+func SayHello(name string) {
+	fmt.Println("你好%s，我是yushuiying，很高兴认识你", name)
 }
